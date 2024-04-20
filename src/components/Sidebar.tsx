429f652408b3da3be8edd85f2aa85ea00a50aa58
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2024-04-20 09:28:46 +0200

    Refactor global CSS and sidebar component
    Refactored the global CSS file to update color variables and added a min-height property to the sidebar component.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = async () => {
   if (session.session === null) return null;
 
   return (
-    <aside className="h-screen bg-muted min-w-52  hidden md:block p-4 pt-8 border-r border-border shadow-inner">
+    <aside className="min-h-screen bg-muted min-w-52  hidden md:block p-4 pt-8 border-r border-border shadow-inner">
       <div className="flex flex-col justify-between h-full">
         <div className="space-y-4 ">
           <h3 className="text-lg font-bold font-mono ml-4">SLEEPCATCH</h3>
