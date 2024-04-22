376f96c6abfc2e424895c5e85d6d120c2e20b09b
Aurélien <2128970+GoldAndLink@users.noreply.github.com>
2024-04-22 03:11:25 +0300

    fix: disable and comment user session for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,22 @@ import { useState, useEffect } from "react";
 import SelectOptionbar from "@/components/SelectOptionbar"
 import Griddy from "@/components/Griddy"
 import Player from "@/components/Player";
-import { AuthSession, getUserAuth } from "@/lib/auth/utils";
+// import { AuthSession, getUserAuth } from "@/lib/auth/utils";
 import { Track } from "@/lib/types";
 
 export default function Home() {
-    const [userAuth, setUserAuth] = useState<AuthSession | null>(null);
+    // const [userAuth, setUserAuth] = useState<AuthSession | null>(null);
     const [track, setTrack] = useState<Track | null>(null);
     const [tracksHash, setTracksHash] = useState<{ [key: string]: Track }>({});
     const [selectedTopic, setSelectedTopic] = useState<string>("world")
 
-    useEffect(() => {
-        const fetchUserAuth = async () => {
-            const auth = await getUserAuth();
-            setUserAuth(auth);
-        };
-        fetchUserAuth();
-    }, []);
+    // useEffect(() => {
+    //     const fetchUserAuth = async () => {
+    //         const auth = await getUserAuth();
+    //         setUserAuth(auth);
+    //     };
+    //     fetchUserAuth();
+    // }, []);
 
     return (
         <main className="space-y-6">
