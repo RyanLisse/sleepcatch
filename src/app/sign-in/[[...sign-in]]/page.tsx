ed835d07dbe2be0fd329efd087e95b528b0a6cc6
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2024-04-25 18:52:55 +0200

    Add Aurora component
    Added a new Aurora component for the sign-in page.

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
+import Aurora from "@/components/ui/Aurora";
 
 export default function Page() {
   return (
+
     <main className="grid place-items-center pt-4">
-        <h1 className="text-3xl font-bold">Welcome to SleepCatch</h1>
-        <p className="text-lg">
-          Sign in to your account to get started.
-        </p>
-      <SignIn />
+        <Aurora />
+
     </main>
+
   );
 }
